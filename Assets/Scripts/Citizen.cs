84ece06cf25d7078f309b395517d74c5ea7732a2
LEGO <matias.labreniuk@davinci.edu.ar>
2024-03-26 13:32:55 -0300

    Initial commit

---
+++ Assets/Scripts/Citizen.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Citizen : MonoBehaviour 
+{
+	//TODO: 
+	//1. Colocar el nodo raíz.
+	//2. Hacer lo que sea necesario para updatear las desiciones
+	//del aldeano.
+#region DONT TOUCH THIS
+	public ParticleSystem sleepPos;
+	public ParticleSystem getWoodPos;
+	public ParticleSystem farmingPos;
+	public ParticleSystem buildPos;
+
+	public void GetFood()
+	{
+		Debug.Log("Decision: Get Food!");
+		DeactivateAllParticles();		
+		SetPosAndPlayParticle(farmingPos);
+	}
+
+	public void GetWood()
+	{
+		Debug.Log("Decision: Get Wood!");
+		DeactivateAllParticles();		
+		SetPosAndPlayParticle(getWoodPos);
+	}
+
+	public void BuildHouses()
+	{
+		Debug.Log("Decision: Build!");
+		DeactivateAllParticles();		
+		SetPosAndPlayParticle(buildPos);
+	}
+
+	public void GoToSleep()
+	{
+		Debug.Log("Decision: Go to Sleep!");
+		DeactivateAllParticles();		
+		SetPosAndPlayParticle(sleepPos);
+	}
+
+	private void DeactivateAllParticles()
+	{
+		sleepPos.Stop();
+		getWoodPos.Stop();
+		farmingPos.Stop();
+		buildPos.Stop();
+	}
+
+	private void SetPosAndPlayParticle(ParticleSystem target)
+	{
+		transform.position = target.transform.position;
+		target.Play();
+	}
+#endregion
+}
---
+++ Assets/Scripts/EnviromentData.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnviromentData : MonoBehaviour
+{
+	//TODO:
+	//Esta clase va a contener toda la información necesaria 
+	//para que el aldeano pueda decidir que hacer (acceden a esta info haciendo algo como
+	//EnviromentData.Instance.foodQty).
+	//Por ejemplo acá pueden poner:
+	//1. Cuanta madera tiene (la necesita para construir)
+	//2. Cuanta comida tiene (la necesita para vivir)
+	//3. Estado del clima (si llueve no debería salir de su casa porque se enferma)
+	//4. Momento del día (de noche es peligroso salir)	
+
+	//La siguiente region les va a servir para
+	//acceder al aldeano desde sus nodos 'Accion' (deberán heredar de los nodos 'Accion'		
+	//y en el método Execute() hacer algo como 'EnviromentData.Instance.citizen.DoSomething()')
+#region DONT TOUCH THIS
+	public Citizen citizen;
+	public static EnviromentData Instance { get; private set; }
+	void Awake()
+	{
+		Instance = this;
+	}
+#endregion
+}
